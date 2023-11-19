9e79c393dc3e26a3119d74733db23e4b48eace5e
Lohanimayank <120321930+Lohanimayank@users.noreply.github.com>
2023-11-19 14:37:54 +0530

    Add files via upload

---
+++ registrationsystem.php
@@ -0,0 +1,19 @@
+from tkinter import*
+import ttk as TTK
+from datetime import date
+from tkinter import filedialog
+from tkinter import messagebox
+from tkinter.ttk import*
+from PIL import Image, ImageTk
+import os
+from tkinter.ttk import combobox
+import openpyxl, xlrd
+from openpyxl import workbook
+import pathlib
+
+root=tk.Tk()
+root.title("registeration system")
+root.geometry("1250x700+210+100")
+root.config()
+
+root.mainloop()
